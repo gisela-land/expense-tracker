11dba1f7bc7e946d80207a0acc87f86b1c7de4be
gisela-land <takeabreathe@hotmail.com>
2021-06-20 18:12:26 +0800

    fix: fix type error

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -6,7 +6,7 @@ const cateArray = [
   { name: '交通出行', icon: '<i class="fas fa-shuttle-van" style="color: lightskyblue;"></i>' },
   { name: '休閒娛樂', icon: '<i class="fas fa-grin-beam" style="color: lightskyblue;"></i>' },
   { name: '餐飲食品', icon: '<i class="fas fa-utensils" style="color: lightskyblue;"></i>' },
-  { name: '其他 ', icon: '<i class="fas fa-pen" style="color: lightskyblue;"></i>' },
+  { name: '其他', icon: '<i class="fas fa-pen" style="color: lightskyblue;"></i>' },
 ]
 
 mongoose.connect('mongodb://localhost/Expense', { useNewUrlParser: true, useUnifiedTopology: true })
